a2aa7b2ddda82c30d6c47a5f3a5b5e82d1421da0
Jason Felds <jafelds@gmail.com>
2009-12-12 13:10:38 -0500

    Start of credits/thanks page. Loading the CSS will come soon.

---
+++ apps/frontend/modules/thanks/templates/indexSuccess.php
@@ -0,0 +1,134 @@
+<h2>Credits and Thanks</h2>
+<p>
+    Let it be known that I don't try to be selfish.  There are many people
+    and places that I have to thank here.
+</p>
+
+<h3 id="top">Options</h3>
+<ul>
+<li><a href="#obvious">The Obvious</a></li>
+<li><a href="#reallife">Real Life</a></li>
+<li><a href="#pumppro">Pump Pro</a></li>
+
+<li><a href="#internet">The Internet</a></li>
+<li><a href="#browser">The Browsers</a></li>
+<li><a href="#promote">The Promoters</a></li>
+</ul>
+
+
+<section id="obvious">
+<h3>The Obvious</h3>
+<dl>
+<dt>Someone up there</dt>
+<dd>This should be obvious.</dd>
+
+<dt>Family</dt>
+<dd>This is also obvious.</dd>
+</dl>
+<p><a href="#top">Back to Top</a></p>
+</section>
+
+
+
+<section id="reallife">
+<h3>Real Life</h3>
+<dl>
+<dt><a href="http://www.ncsu.edu">North Carolina State University</a></dt>
+
+<dd>They gave me the majority of my education.</dd>
+<dt>Jboy AAA</dt>
+<dd>He has helped with private testing on the website, and also had a machine available that I could use.</dd>
+<dt>BAMABoxy</dt>
+<dd>General good guy, he's provided friendship and support. Part of the website is in dedication to him.</dd>
+</dl>
+<p><a href="#top">Back to Top</a></p>
+</section>
+
+
+
+<section id="pumppro">
+<h3>Pump Pro</h3>
+<dl>
+<dt><a href="http://www.piupro.com">Pump It Up: Pro</a></dt>
+<dd>Without this game, this website would not have existed.</dd>
+<dt><a href="http://www.andamiro.com">ANDAMIRO Entertainment</a></dt>
+<dd>They made the original Pump It Up series, and are still going strong with NX:A.</dd>
+<dt><a href="http://www.kyle-ward.com">Kyle Ward</a></dt>
+<dd>It was Kyle that helped make me realize the potential of such a web site.</dd>
+<dt>Ryan McKanna</dt>
+
+<dd>He has kept my head cool in previous endeavors, so I thank him again here.</dd>
+<dt>Jason Dread</dt>
+<dd>Another nice guy, he has brought traffic to this page in the past.</dd>
+<dt>The other Pump Pro/StepMania Staffers and Developers</dt>
+<dd>Too many to name, but all have contribtued to the Pump It Up: Pro engine in their own way.</dd>
+</dl>
+<p><a href="#top">Back to Top</a></p>
+</section>
+
+
+
+<section id="internet">
+<h3>The Internet</h3>
+<dl>
+<dt>VxJasonxV</dt>
+<dd>He provided the original website space, and is a great PHP troubleshooter.</dd>
+<dt>Rathstar</dt>
+<dd>He was an excellent guide for the step chart generator in use.</dd>
+<dt>Kyrandian</dt>
+<dd>Another beta tester, he found some bugs that I was able to fix.</dd>
+<dt>dasbacon</dt>
+
+<dd>His videos and statistics were of big help for me.</dd>
+<dt>Hatena Zubon</dt>
+<dd>All of his edits, especially for Hasse Mich, proved useful.</dd>
+<dt><a href="http://techpatterns.com/downloads/php_browser_detection.php">TechPatterns' PHP &amp; OS Detection Scripts</a></dt>
+<dd>My original source for user agent switching, their code is useful on any PHP page.</dd>
+<dt><a href="http://www.pylonshq.com">Pylons</a></dt>
+<dd>This redesign would not have taken place if it wasn't for this Python framework.</dd>
+<dt><a href="http://www.sqlalchemy.org/">SQLAlchemy</a></dt>
+
+<dd>Database queries became simpler with SQLAlchemy.  They also became more fun.</dd>
+</dl>
+<p><a href="#top">Back to Top</a></p>
+</section>
+
+
+
+<section id="browser">
+<h3>The Browsers</h3>
+<dl>
+<dt><a href="http://www.firefox.com">Firefox</a></dt>
+<dd>A great web browser, it offers many add-ons/extensions for both casual and professional users.</dd>
+
+<dt><a href="http://www.apple.com/safari">Safari</a></dt>
+<dd>Apple's entry, it is sleek while also having powerful Inspectors.</dd>
+<dt><a href="http://www.opera.com">Opera</a></dt>
+<dd>While not many use this web browser, it has helped with web testing in the past.</dd>
+<dt><a href="http://www.google.com/chrome">Chrome</a></dt>
+<dd>Google's entry uses the same engine as Safari, but offers its own bells and whistles.</dd>
+<dt><a href="http://www.elinks.or.cz">ELinks</a></dt>
+<dd>An excellent text browser, ELinks helped confirm the website was functional with just text.</dd>
+<dt><a href="http://lynx.isc.org">Lynx</a></dt>
+
+<dd>One of the original text browsers, Lynx also helped with the text based previewing.</dd>
+</dl>
+<p><a href="#top">Back to Top</a></p>
+</section>
+
+
+
+<section id="promote">
+<h3>The Promoters</h3>
+<dl>
+<dt><a href="http://www.site5.com">Site5 Web Hosting</a></dt>
+<dd>The original host for this page, they've been good to me.</dd>
+
+<dt><a href="http://www.slicehost.com">Slicehost - VPS Hosting</a></dt>
+<dd>My current host, they give me a lot of control at a reasonable rate.</dd>
+<dt><a href="http://www.piu-pro.com">Pump It Up Pro Blog</a></dt>
+<dd>They were one of the first fan websites to link to back here.</dd>
+</dl>
+<p><a href="#top">Back to Top</a></p>
+</section>
+
