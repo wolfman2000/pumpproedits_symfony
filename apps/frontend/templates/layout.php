be4a75761d69d98a483da9f8140c3209512afb5f
Jason Felds <jafelds@gmail.com>
2009-12-12 10:57:45 -0500

    Implement some HTML5 and non ASCII characters. This content will be served as UTF-8: only the fonts have to be gotten. Or...do they...?

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -3,12 +3,19 @@
   <head>
     <?php include_http_metas() ?>
     <?php include_metas() ?>
-    <?php include_title() ?>
+    <title>Pump Pro Edits</title>
     <link rel="shortcut icon" href="/favicon.ico" />
     <?php include_stylesheets() ?>
     <?php include_javascripts() ?>
   </head>
   <body>
-    <?php echo $sf_content ?>
+    <header>Pump Pro Edits</header>
+    <article>
+      <?php echo $sf_content ?>
+    </article>
+    <nav>
+      <p>Put primary navigation in here.</p>
+    </nav>
+    <footer>This website is © Jason “Wolfman2000” Felds</footer>
   </body>
 </html>
