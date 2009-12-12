596ff88ffc7d157a5466d818daf7ea52e68c830b
Jason Felds <jafelds@gmail.com>
2009-12-12 13:45:30 -0500

    Start of base edits page.

--- apps/frontend/modules/base/actions/actions.class.php
+++ apps/frontend/modules/base/actions/actions.class.php
@@ -17,6 +17,6 @@ class baseActions extends sfActions
   */
   public function executeIndex(sfWebRequest $request)
   {
-    $this->forward('default', 'module');
+    # $this->forward('default', 'module');
   }
 }
---
+++ apps/frontend/modules/base/templates/indexSuccess.php
@@ -0,0 +1,24 @@
+<?php
+  slot('title', 'Base Edit Files Page ?? — Pump Pro Edits');
+?> 
+<h2>Base Edit Files</h2> 
+<p>
+	Edit files are made with a .edit extension, and placed in
+	the appropriate folder on your USB drive when you go play
+	Pump It Up Pro.
+</p>
+<p>
+	At times you may want to edit a song's steps, but don't
+	exactly know what measure they start on or how long
+	the song lasts.  The base edit files below are
+	provided as a convenience for edit makers to have
+	a place to start.
+</p>
+<p>
+	These are so useful, even some of
+	the developers of the game use these files.  Surely
+	they will work for you!  Just download the single
+	and/or double steps of the files you want.
+</p>
+
+<p>Actual content to come soon.</p>
