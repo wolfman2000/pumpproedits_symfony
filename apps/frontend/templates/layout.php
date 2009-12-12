e6121c804ff2af0bd51759ebd1f8db28f96f5723
Jason Felds <jafelds@gmail.com>
2009-12-12 12:33:43 -0500

    Port over nav content. Soft code this later.

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -14,8 +14,45 @@
       <?php echo $sf_content ?>
     </article>
     <nav>
-      <p>Put primary navigation in here.</p>
+      <p>Fix this!</p>
+      <p>
+<a href="/login">Log In</a>
+or <a href="/register">Register</a>?
+</p>
+<ul>
+<li>
+    <h4>Members</h4>
+    <ul>
+
+    <li><a href="/register">Register</a></li>
+    <li><a href="/confirm">Confirm Account</a></li>
+    <li><a href="/login">Log In</a></li>
+    <li><a href="/help">Account Help</a></li>
+    <li><a href="/reset">Reset Password</a></li>
+    </ul>
+
+</li>
+<li>
+    <h4>Edits</h4>
+    <ul>
+    <li><a href="/base">Base Edits</a></li>
+    <li><a href="/stats">Edit Stat Getter</a></li>
+    <li><a href="/edits">Edit List</a></li>
+    </ul>
+
+</li>
+<li>
+    <h4>Everyone</h4>
+    <ul>
+    <li><a href="/news">Previous Updates</a></li>
+    <li><a href="/contact">Contact</a></li>
+    <li><a href="/credits">Credits/Thanks</a></li>
+    </ul>
+
+</li>
+</ul>
+
     </nav>
-    <footer>This website is © Jason “Wolfman2000” Felds</footer>
+    <footer>This website is © <a href="mailto:jafelds@gmail.com">Jason “Wolfman2000” Felds</a></footer>
   </body>
 </html>
