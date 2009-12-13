d373230c44e53dadb48594b9f17eb338af65e27d
Jason Felds <jafelds@gmail.com>
2009-12-12 21:08:23 -0500

    Our new module for the form.

---
+++ apps/frontend/modules/stats/actions/actions.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * stats actions.
+ *
+ * @package    pumpproedits
+ * @subpackage stats
+ * @author     JasonWolfman2000Felds
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class statsActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->forward('default', 'module');
+  }
+}
