16d40a561a2687866a2b44d0a4641f16e07ba7a3
Jason Felds <jafelds@gmail.com>
2009-12-12 22:30:58 -0500

    Use older format for now.

--- apps/frontend/modules/stats/templates/_form.php
+++ apps/frontend/modules/stats/templates/_form.php
@@ -1,6 +1,7 @@
 <?php include_stylesheets_for_form($form) ?>
 <?php include_javascripts_for_form($form) ?>
  
-<?php echo form_tag_for($form, '@edit_stat_post') ?>
+<form action="<?php echo url_for('@edit_stat_post') ?>" method="POST">
   <?php echo $form ?>
 </form>
+
