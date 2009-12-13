4d40563b561f9a6e9f366529ef0feb01e7b0636b
Jason Felds <jafelds@gmail.com>
2009-12-12 21:26:37 -0500

    Separate, use proper schemas. Prepare to revert this.

--- lib/form/ValidateEditForm.class.php
+++ lib/form/ValidateEditForm.class.php
@@ -4,7 +4,11 @@ class ValidateEditForm extends sfForm
 {
   public function configure()
   {
-    $this->setWidgets(array('file' => new sfWidgetFormInputFile() ));
+    $val = array('label' => 'file');
+    $this->widgetSchema['file'] = new sfWidgetFormInputFile($val);
+    $val = array('required' => true, 'path' => sfConfig::get('sf_upload_dir'));
+    $this->validationSchema['file'] = new sfValidatorFile($val);
+#    $this->setWidgets(array('file' => new sfWidgetFormInputFile() ));
     $this->setWidgets->setFormFormatterName('list');
   }
 }
