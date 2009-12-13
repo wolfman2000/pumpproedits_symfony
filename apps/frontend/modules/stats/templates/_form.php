f2c5ca0e180a2e33723c2f26d41aeea2104ea1df
Jason Felds <jafelds@gmail.com>
2009-12-12 21:45:55 -0500

    Proper routing for the form.

--- apps/frontend/modules/stats/templates/_form.php
+++ apps/frontend/modules/stats/templates/_form.php
@@ -1,6 +1,6 @@
 <?php include_stylesheets_for_form($form) ?>
 <?php include_javascripts_for_form($form) ?>
  
-<?php echo form_tag_for($form, '@job') ?>
+<?php echo form_tag_for($form, '@edit_stat_post') ?>
   <?php echo $form ?>
 </form>
