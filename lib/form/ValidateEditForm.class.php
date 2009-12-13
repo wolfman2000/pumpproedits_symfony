08fdc553f7907a616da2f5c23b95c8a69a9dba99
Jason Felds <jafelds@gmail.com>
2009-12-12 22:28:20 -0500

    Stick with one format for now. Look at history for the old (bad?) way.

--- lib/form/ValidateEditForm.class.php
+++ lib/form/ValidateEditForm.class.php
@@ -4,11 +4,9 @@ class ValidateEditForm extends sfForm
 {
   public function configure()
   {
-    $val = array('label' => 'file');
-    $this->widgetSchema['file'] = new sfWidgetFormInputFile($val);
-    $val = array('required' => true, 'path' => sfConfig::get('sf_upload_dir'));
-    $this->validationSchema['file'] = new sfValidatorFile($val);
-#    $this->setWidgets(array('file' => new sfWidgetFormInputFile() ));
-#    $this->setWidgets->setFormFormatterName('list');
+    $this->setWidgets(array(
+      'file' => new sfWidgetFormInputFile()
+    ));
+    $this->widgetSchema->setFormFormatterName('list');
   }
 }
