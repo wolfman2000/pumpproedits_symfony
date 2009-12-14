4a0dde4b7a81768f33f1a2b08a8caea930bc1ba0
Jason Felds <jafelds@gmail.com>
2009-12-14 17:28:44 -0500

    Implement base signin/signout functions. Improvements to come soon (possibly).

--- lib/PPEUser.class.php
+++ lib/PPEUser.class.php
@@ -2,5 +2,15 @@
 
 class PPEUser extends sfBasicSecurityUser
 {
+  public function signIn($credentials)
+  {
+    $this->setAuthenticated(true);
+    $this->addCredentials($credentials);
+  }
 
+  public function signOut()
+  {
+    $this->setAuthenticated(false);
+    $this->clearCredentials();
+  }
 }
