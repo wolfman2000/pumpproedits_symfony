7b9c436cdcb902f87b37d9d11687b7944b107119
Jason Felds <jafelds@gmail.com>
2009-12-15 18:43:13 -0500

    Copied ValidateEditForm to here. The contents will be changed later.

---
+++ lib/form/RegisterForm.class.php
@@ -0,0 +1,23 @@
+<?php
+
+class RegisterForm extends sfForm
+{
+  public function configure()
+  {
+    $this->setWidgets(array(
+      'file' => new sfWidgetFormInputFile()
+    ));
+    $decorator = new myWidgetFormSchemaFormatterDList($this->getWidgetSchema());
+    $this->widgetSchema->addFormFormatter('dlist', $decorator);
+    $this->widgetSchema->setFormFormatterName('dlist');
+    $size = sfConfig::get('app_max_edit_file_size');
+    $validate['max_size'] = $size;
+    $validate['path'] = sfConfig::get('sf_upload_dir');
+    $messages['required'] = 'You must submit an edit file!';
+    $messages['max_size'] = sprintf("The edit must be less than %d bytes!", $size);
+    $vfile = new sfValidatorFile($validate, $messages);
+    $val['file'] = $vfile;
+    $this->setValidators($val);
+    $this->widgetSchema->setNameFormat('validate[%s]');
+  }
+}
