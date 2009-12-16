10a49fef1778b2ee2a1b7d88d9aaa9ad28b31f4c
Jason Felds <jafelds@gmail.com>
2009-12-15 19:30:09 -0500

    Warnings gone, but still more to go. Better get the thing to display.

--- lib/form/RegisterForm.class.php
+++ lib/form/RegisterForm.class.php
@@ -7,21 +7,23 @@ class RegisterForm extends sfForm
     $max_un = sfConfig::get('app_max_username_length');
     $min_un = sfConfig::get('app_min_username_length');
     $min_pw = sfConfig::get('app_min_password_length');
+    $max_em = sfConfig::get('app_max_email_length');
     $unreq['maxlength'] = $max_un;
-    $pieces['username'] = new sfWidgetFormInput(null, $unreq);
-    $pieces['password'] = new sfWidgetFormInput();
-    $pieces['passdual'] = new sfWidgetFormInput();
+    $pieces['username'] = new sfWidgetFormInput(array(), $unreq);
+    $pieces['password'] = new sfWidgetFormInput(array());
+    $pieces['passdual'] = new sfWidgetFormInput(array());
     $unreq['maxlength'] = sfConfig::get('app_max_email_length');
-    $pieces['email'] = new sfWidgetFormInput(null, $unreq);
+    $pieces['email'] = new sfWidgetFormInput(array(), $unreq);
     $this->setWidgets($pieces);
     $decorator = new myWidgetFormSchemaFormatterDList($this->getWidgetSchema());
     $this->widgetSchema->addFormFormatter('dlist', $decorator);
     $this->widgetSchema->setFormFormatterName('dlist');
+    $this->widgetSchema->setNameFormat('validate[%s]');
 
-    $unerr['required'] = "You must supply an email address.";
-    $val['email'] = new sfValidatorEmail(array(), $unerr);
+//    $unerr['required'] = "You must supply an email address.";
+//    $val['email'] = new sfValidatorEmail(array(), $unerr);
 
-
+/*
     $unerr['min_length'] = $min_un;
     $messs['min_length'] = "Your username must be at least $min_un characters.";
     $unerr['max_length'] = $min_un;
@@ -40,8 +42,11 @@ class RegisterForm extends sfForm
     $messs['required'] = "You must supply a confirmation password.";
 
     $val['passdual'] = new sfValidatorString($unerr, $messs);
+*/
+    $this->setValidators(array(
+      'email'   => new sfValidatorEmail(array('max_length' => $max_em), array('required' => "You must supply an email address."))
+    ));
 
-    $this->setValidators($val);
-    $this->widgetSchema->setNameFormat('validate[%s]');
+//    $this->setValidators(array($val));
   }
 }
