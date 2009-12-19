f56558e6461c6c64068c403732f29156d61037b1
Jason Felds <jafelds@gmail.com>
2009-12-18 23:55:41 -0500

    Add new function. Hopefully this will be useful.

--- lib/model/doctrine/PPE_User_UserTable.class.php
+++ lib/model/doctrine/PPE_User_UserTable.class.php
@@ -2,4 +2,9 @@
 
 class PPE_User_UserTable extends Doctrine_Table
 {
+  public function getIDByEmail($email)
+  {
+    return $this->createQuery('a')->select('id')->
+      where('email = ?', $email)->execute();
+  }
 }
