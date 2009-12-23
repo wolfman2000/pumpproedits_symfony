3560a2b3e19839434aa204c03b258b2654b5f567
Jason Felds <jafelds@gmail.com>
2009-12-22 23:06:49 -0500

    Revert "Add function." (wrong file)
    This reverts commit 4b70f2672f70b3fdcaa08faf9cfa15db2864988d.

--- lib/model/doctrine/PPE_User_PowerTable.class.php
+++ lib/model/doctrine/PPE_User_PowerTable.class.php
@@ -2,10 +2,4 @@
 
 class PPE_User_PowerTable extends Doctrine_Table
 {
-  public function getIsUserBanned($id)
-  {
-    $query = $this->createQuery('a')->select('ur.role');
-    $query = $query->innerJoin('a.PPE_User_Role ur')->whereIn('role', array('banned', 'forbidden'));
-    return $query->execute();
-  }
 }
