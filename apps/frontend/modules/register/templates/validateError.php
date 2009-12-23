0d038397d463287deb0f6f08ceac66bfd51944e8
Jason Felds <jafelds@gmail.com>
2009-12-22 20:59:55 -0500

    Add slotted title.

--- apps/frontend/modules/register/templates/validateError.php
+++ apps/frontend/modules/register/templates/validateError.php
@@ -1,4 +1,6 @@
-<p>Error!</p>
+<?php slot('title', 'Registration Failure — Pump Pro Edits') ?>
+
+<h2>Error!</h2>
 <p>Please check the form and try again.</p>
 
 <?php include_partial("register/form", array('form' => $form)) ?>
