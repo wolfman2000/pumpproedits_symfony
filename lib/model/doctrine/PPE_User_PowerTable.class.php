4b70f2672f70b3fdcaa08faf9cfa15db2864988d
Jason Felds <jafelds@gmail.com>
2009-12-22 23:03:42 -0500

    Add function.

--- lib/model/doctrine/PPE_User_PowerTable.class.php
+++ lib/model/doctrine/PPE_User_PowerTable.class.php
@@ -2,4 +2,10 @@
 
 class PPE_User_PowerTable extends Doctrine_Table
 {
+  public function getIsUserBanned($id)
+  {
+    $query = $this->createQuery('a')->select('ur.role');
+    $query = $query->innerJoin('a.PPE_User_Role ur')->whereIn('role', array('banned', 'forbidden'));
+    return $query->execute();
+  }
 }
