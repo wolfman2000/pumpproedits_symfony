819695e44534c0cb18231d150708b2658d669493
Jason Felds <jafelds@gmail.com>
2009-12-22 22:02:25 -0500

    Add function.

--- lib/model/doctrine/PPE_User_UserTable.class.php
+++ lib/model/doctrine/PPE_User_UserTable.class.php
@@ -7,4 +7,10 @@ class PPE_User_UserTable extends Doctrine_Table
     return $this->createQuery('a')->select('id')->
       where('email = ?', $email)->execute();
   }
+  
+  public function getIDByUser($user)
+  {
+    return $this->createQuery('a')->select('id')->
+      where('name = ?', $user)->execute();
+  }
 }
