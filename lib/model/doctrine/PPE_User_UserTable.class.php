7ced5f3ff1dd40a6e1ace1ed8177e7f2bd0de685
Jason Felds <jafelds@gmail.com>
2009-12-23 18:25:41 -0500

    Use slightly inefficient syntax. Guarantee that the right answer results.

--- lib/model/doctrine/PPE_User_UserTable.class.php
+++ lib/model/doctrine/PPE_User_UserTable.class.php
@@ -4,13 +4,15 @@ class PPE_User_UserTable extends Doctrine_Table
 {
   public function getIDByEmail($email)
   {
-    return $this->createQuery('a')->select('id')->
-      where('email = ?', $email)->execute();
+    $q = $this->createQuery('a')->select('id')->where('email = ?', $email);
+    $q = $q->fetchOne(array(), Doctrine_Core::HYDRATE_ARRAY);
+    return $q['id'];
   }
   
   public function getIDByUser($user)
   {
-    return $this->createQuery('a')->select('id')->
-      where('name = ?', $user)->execute();
+    $q = $this->createQuery('a')->select('id')->where('name = ?', $user);
+    $q = $q->fetchOne(array(), Doctrine_Core::HYDRATE_ARRAY);
+    return $q['id'];
   }
 }
