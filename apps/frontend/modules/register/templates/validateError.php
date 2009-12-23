ce25e4f396c9afd79fb99f0242670e80b1dc61c8
Jason Felds <jafelds@gmail.com>
2009-12-22 23:03:04 -0500

    Cleaner finish.

--- apps/frontend/modules/register/templates/validateError.php
+++ apps/frontend/modules/register/templates/validateError.php
@@ -1,6 +1,15 @@
 <?php slot('title', 'Registration Failure — Pump Pro Edits') ?>
-
 <h2>Error!</h2>
+<?php if (isset($data)): ?>
+<p>There was a problem with processing the data.</p>
+<ul>
+<?php foreach ($data as $d): ?>
+<li><?php echo $d ?></li>
+<?php endforeach; ?>
+</ul>
+<?php else: ?>
+<p>The errors are listed inside the form.</p>
+<?php endif; ?>
 <p>Please check the form and try again.</p>
 
-<?php include_partial("register/form", array('form' => $form)) ?>
+<?php include_partial("register/form", array('form' => $form));
