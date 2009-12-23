0deac429614d01facaf12b859fe2a05321e210b7
Jason Felds <jafelds@gmail.com>
2009-12-22 23:07:04 -0500

    Add function to RIGHT table.

--- lib/model/doctrine/PPE_User_RoleTable.class.php
+++ lib/model/doctrine/PPE_User_RoleTable.class.php
@@ -2,4 +2,10 @@
 
 class PPE_User_RoleTable extends Doctrine_Table
 {
+  public function getIsUserBanned($id)
+  {
+    $query = $this->createQuery('a')->select('role');
+    $query = $query->innerJoin('a.PPE_User_Powers')->whereIn('role', array('banned', 'forbidden'));
+    return $query->execute();
+  }
 }
