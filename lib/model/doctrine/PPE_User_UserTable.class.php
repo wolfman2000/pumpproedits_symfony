5cdd30db3dbedb5306b911a5847a37dcd3a52492
Jason Felds <jafelds@gmail.com>
2009-12-24 12:12:20 -0500

    Add function. Fix casing.

--- lib/model/doctrine/PPE_User_UserTable.class.php
+++ lib/model/doctrine/PPE_User_UserTable.class.php
@@ -4,15 +4,22 @@ class PPE_User_UserTable extends Doctrine_Table
 {
   public function getIDByEmail($email)
   {
-    $q = $this->createQuery('a')->select('id')->where('email = ?', $email);
+    $q = $this->createQuery('a')->select('id')->where('email = ?', strtolower($email));
     $q = $q->fetchOne(array(), Doctrine_Core::HYDRATE_ARRAY);
     return $q['id'];
   }
   
   public function getIDByUser($user)
   {
-    $q = $this->createQuery('a')->select('id')->where('lc_name = ?', $user);
+    $q = $this->createQuery('a')->select('id')->where('lc_name = ?', strtolower($user));
     $q = $q->fetchOne(array(), Doctrine_Core::HYDRATE_ARRAY);
     return $q['id'];
+  }
+  
+  public function getConfirmedByID($id)
+  {
+    $q = $this->createQuery('a')->select('is_confirmed')->where('id = ?', $id);
+    $q = $q->fetchOne(array(), Doctrine_Core::HYDRATE_ARRAY);
+    return $q['is_confirmed'];
   }
 }
