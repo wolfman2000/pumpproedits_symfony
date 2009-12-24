635338f116d604fc48b84dc866e6d308c212b85e
Jason Felds <jafelds@gmail.com>
2009-12-23 22:50:35 -0500

    Some implementation of behavior. Incomplete. I need assistance with getting the length.

--- lib/behaviors/Lowercase.class.php
+++ lib/behaviors/Lowercase.class.php
@@ -2,9 +2,16 @@
 
 class Lowercase extends Doctrine_Template
 {
+  protected $_columns = array("columns");
+
   public function setTableDefinition()
   {
-
+    foreach (preg_split("/\[\]\s,~", $this->_options['columns']) as $column)
+    {
+      $columnName = "lc_" . $column;
+      $table = $this->_table->getTable();
+      $table->setColumn($columnName, 'string', null);
+    }
   }
 
   public function setUp()
