349aaea70c0a8ddf8b27df53c74b8cfa0c8cc777
Jason Felds <jafelds@gmail.com>
2009-12-25 13:57:58 -0500

    Actual content.

---
+++ apps/frontend/modules/contact/templates/indexSuccess.php
@@ -0,0 +1,11 @@
+<?php slot('title', 'Contact the Webmaster — Pump Pro Edits');
+slot('h2', '<h2>Contact the Webmaster</h2>'); ?>
+
+<p>If you need to reach the web master for any reason, you have
+two easy ways of doing so. Those familiar with sending email
+through client programs such as Apple's Mail or Mozilla Thunderbird
+can <a href="mailto:jafelds@gmail.com">email me directly.</a> A
+form alternative is also provided below. Use whichever is convenient
+for you.</p>
+
+<?php include_partial("contact/form", array('form' => $form)) ?>
