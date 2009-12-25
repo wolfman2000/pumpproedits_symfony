4801c0222f2ab7f59b3862910123cda2c9a20bc1
Jason Felds <jafelds@gmail.com>
2009-12-25 10:20:00 -0500

    Add IE specific code.

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -7,9 +7,10 @@ include(sfConfig::get('sf_lib_dir') . "/browser_detect.php"); ?>
 <title><?php include_slot('title', 'Pump Pro Edits') ?></title>
 <link rel="shortcut icon" href="/favicon.ico" />
 <?php include_stylesheets();
-if (browser_detection(7) == "ie"):
-
-endif;
+if (browser_detection(7) == "ie"): ?>
+<script type="text/javascript" src="js/IE8.js"></script>
+<script type="text/javascript" src="js/ie_html5.js"></script>
+<?php endif;
 include_javascripts(); ?>
 </head>
 <body>
