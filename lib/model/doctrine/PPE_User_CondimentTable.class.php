5bee5ef30d7ae2e39dcf29826cf588446c2c6567
Jason Felds <jafelds@gmail.com>
2009-12-25 00:33:16 -0500

    Add functions. Refactor code.

--- lib/model/doctrine/PPE_User_CondimentTable.class.php
+++ lib/model/doctrine/PPE_User_CondimentTable.class.php
@@ -2,18 +2,29 @@
 
 class PPE_User_CondimentTable extends Doctrine_Table
 {
+  private function checkPassword($salt, $pass)
+  {
+    $pepper = hash("sha256", $pass . $row['salt']);
+    $q = $this->createQuery('a')->select('id')->where('pepper = ?', $pepper)
+      ->fetchOne(array(), Doctrine_Core::HYDRATE_ARRAY);
+    return $q['id'];
+  }
+
   public function confirmUser($oregano, $pass)
   {
-    $q = $this->createQuery('a')->select('salt')->where('oregano = ?', $oregano);
-    $row = $q->fetchOne(array(), Doctrine_Core::HYDRATE_ARRAY);
+    $q = $this->createQuery('a')->select('salt')
+      ->where('oregano = ?', $oregano)
+      ->fetchOne(array(), Doctrine_Core::HYDRATE_ARRAY);
     
-    if ($row == false) { return false; }
+    return ($q ? $this->checkPassword($q['salt'], $pass) : false);
+  }
+  
+  public function checkUser($name, $pass)
+  {
+    $q = $this->createQuery('a')->select('salt')
+      ->innerJoin('a.PPE_User_Users b')->where('b.name = ?', $name)
+      ->fetchOne(array(), Doctrine_Core::HYDRATE_ARRAY);
     
-    $pepper = hash("sha256", $pass . $row['salt']);
-    
-    $q = $this->createQuery('a')->select('id')->where('pepper = ?', $pepper);
-    $q = $q->fetchOne(array(), Doctrine_Core::HYDRATE_ARRAY);
-    return $q['id'];
-    
+    return ($q ? $this->checkPassword($q['salt'], $pass) : false);
   }
 }
