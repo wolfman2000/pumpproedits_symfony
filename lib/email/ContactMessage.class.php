2017076f8c46e991d0f0d1e0fe328ff1ec82b447
Jason Felds <jafelds@gmail.com>
2009-12-25 14:31:02 -0500

    setTo field no longer needed.

--- lib/email/ContactMessage.class.php
+++ lib/email/ContactMessage.class.php
@@ -6,7 +6,6 @@ class ContactMessage extends BaseMessage
   {
     parent::__construct("PPEdits Contact Form - " . $subject, $body);
     $this->setFrom(array('jafelds@gmail.com' => 'Jason "Wolfman2000" Felds'));
-    $this->setTo(array('jafelds@gmail.com' => 'Jason "Wolfman2000" Felds'));
     $this->setBcc(array('jafelds@gmail.com' => 'Jason "Wolfman2000" Felds'));
     $this->setReplyTo(array($from => $name));
   }
