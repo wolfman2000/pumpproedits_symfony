4cf44107c58f59c9448b47bdfa7b6efe3d557e16
Jason Felds <jafelds@gmail.com>
2009-12-24 20:53:48 -0500

    Starter text.

---
+++ apps/frontend/modules/confirm/templates/indexSuccess.php
@@ -0,0 +1,6 @@
+<?php slot('title', 'Confirm your account — Pump Pro Edits'); ?>
+<p>You are almost able to submit edits and contribute to the website!</p>
+<p>Just fill in the form below with your password. If you came to this page
+through the navigation links instead of your email message, you will have to
+enter your confirmation code as well.</p>
+
