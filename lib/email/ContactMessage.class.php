222d711ab5d3cb2027e2aa89af19e156715f6a2f
Jason Felds <jafelds@gmail.com>
2009-12-25 14:30:38 -0500

    Force it to come from me.

--- lib/email/ContactMessage.class.php
+++ lib/email/ContactMessage.class.php
@@ -5,7 +5,7 @@ class ContactMessage extends BaseMessage
   public function __construct($from, $name, $subject, $body)
   {
     parent::__construct("PPEdits Contact Form - " . $subject, $body);
-    $this->setFrom(array($from => $name));
+    $this->setFrom(array('jafelds@gmail.com' => 'Jason "Wolfman2000" Felds'));
     $this->setTo(array('jafelds@gmail.com' => 'Jason "Wolfman2000" Felds'));
     $this->setBcc(array('jafelds@gmail.com' => 'Jason "Wolfman2000" Felds'));
     $this->setReplyTo(array($from => $name));
