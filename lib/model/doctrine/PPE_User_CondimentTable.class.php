e813fe10423d648cef70f28c6ffefebd425f328d
Jason Felds <jafelds@gmail.com>
2009-12-24 22:51:13 -0500

    Add confirmUser function. Two queries are in use to be database agnostic. It's probably more important to be fair to all.

--- lib/model/doctrine/PPE_User_CondimentTable.class.php
+++ lib/model/doctrine/PPE_User_CondimentTable.class.php
@@ -2,4 +2,18 @@
 
 class PPE_User_CondimentTable extends Doctrine_Table
 {
+  public function confirmUser($oregano, $pass)
+  {
+    $q = $this->createQuery('a')->select('salt')->where('oregano = ?', $oregano);
+    $row = $q->fetchOne(array(), Doctrine_Core::HYDRATE_ARRAY);
+    
+    if ($row == false) { return false; }
+    
+    $pepper = hash("sha256", $pass . $row['salt']);
+    
+    $q = $this->createQuery('a')->select('id')->where('pepper = ?', $pepper);
+    $q = $q->fetchOne(array(), Doctrine_Core::HYDRATE_ARRAY);
+    return $q['id'];
+    
+  }
 }
