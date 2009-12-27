2578b4e0572c907a3cce7b462e85964fa1f46d2a
Jason Felds <jafelds@gmail.com>
2009-12-27 01:41:16 -0500

    The start: just generating an arrow for now. More work will go into this tomorrow.

--- apps/frontend/modules/xmltest/actions/actions.class.php
+++ apps/frontend/modules/xmltest/actions/actions.class.php
@@ -19,11 +19,26 @@ class xmltestActions extends sfActions
   {
     $xml = new DomDocument("1.0", "UTF-8");
     $node = $xml->createElement('svg');
+    
     $xml->preserveWhiteSpace = false;
     $xml->formatOutput = true;
     $css = $xml->createProcessingInstruction('xml-stylesheet','type="text/css" href="/css/_svg.css"');
     $xml->appendChild($css);
     $xml->appendChild($node);
+    
+    $node->setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+    $node->setAttribute('xmlns:xlink', 'http://www.w3.org/1999/xlink');
+    $node->setAttribute('version', '1.1');
+    
+    $use = $xml->createElement('use');
+    $use->setAttribute('x', '0');
+    $use->setAttribute('y', '0');
+    $use->setAttribute('xlink:href', '/svg/arrowdef.svg#CNarrow');
+    $use->setAttribute('class', 'note_008');
+    $node->appendChild($use);
+    
+    
+    
     $response = $this->getResponse();
     $response->clearHttpHeaders();
     $response->setHttpHeader('Content-Type', 'application/xml');
