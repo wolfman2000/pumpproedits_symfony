002c35f17c5f922298f002aed40323a49c7aada4
Jason Felds <jafelds@gmail.com>
2009-12-26 23:48:27 -0500

    Add temp module. This will most likely go away once it is figured out whether or not XML charts will be feasible.

---
+++ apps/frontend/modules/xmltest/actions/actions.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * xmltest actions.
+ *
+ * @package    pumpproedits
+ * @subpackage xmltest
+ * @author     JasonWolfman2000Felds
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class xmltestActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->forward('default', 'module');
+  }
+}
