17d95fd7193f1289a05f27fe3066b0e5a54f3233
Jason Felds <jafelds@gmail.com>
2009-12-27 12:22:01 -0500

    Fix routing/copy/paste.

--- apps/frontend/modules/chart/actions/actions.class.php
+++ apps/frontend/modules/chart/actions/actions.class.php
@@ -8,7 +8,7 @@
  * @author     JasonWolfman2000Felds
  * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
-class statsActions extends sfActions
+class chartActions extends sfActions
 {
  /**
   * Executes index action
