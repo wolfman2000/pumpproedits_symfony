76ab22ecb52cfa26fb8b2fd7027a625829a03898
Jason Felds <jafelds@gmail.com>
2009-12-27 12:07:36 -0500

    Use the stat module's functions.

--- apps/frontend/modules/chart/actions/actions.class.php
+++ apps/frontend/modules/chart/actions/actions.class.php
@@ -8,7 +8,7 @@
  * @author     JasonWolfman2000Felds
  * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
-class chartActions extends sfActions
+class statsActions extends sfActions
 {
  /**
   * Executes index action
@@ -17,6 +17,45 @@ class chartActions extends sfActions
   */
   public function executeIndex(sfWebRequest $request)
   {
-    $this->forward('default', 'module');
+    $this->form = new ValidateEditForm();
+  }
+ /**
+  * Executes validate action (form required)
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeValidate(sfWebRequest $request)
+  {
+    $this->form = new ValidateEditForm();
+    $this->form->bind($request->getParameter('validate'), $request->getFiles('validate'));
+    $errors = array();
+    if ($this->form->isValid())
+    {
+      $file = $this->form->getValue('file');
+      $filename = 'uploaded'.sha1($file->getOriginalName());
+      $extension = $file->getExtension($file->getOriginalExtension());
+      $path = sfConfig::get('sf_upload_dir').'/'.$filename.$extension;
+      $file->save($path);
+
+      /* File validation takes place here. */
+      $tmp = new EditParser();
+      try
+      {
+        $this->page = "parseyay";
+        $this->result = $tmp->get_stats(fopen($path, "r"));
+      }
+      catch (sfParseException $e)
+      {
+        $this->page = "parseboo";
+        $this->result = $e;
+      }
+      /* Do this step at the end. */
+      @unlink($path);
+    }
+    else
+    {
+      $this->getResponse()->setStatusCode(409);
+      return sfView::ERROR;
+    }
   }
 }
