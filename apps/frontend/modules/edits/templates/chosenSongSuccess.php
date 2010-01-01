2ae6717f278f3b7a0028e322aa8e3f299e81ea58
Jason Felds <jafelds@gmail.com>
2010-01-01 11:31:48 -0500

    Display mini table to start.

--- apps/frontend/modules/edits/templates/chosenSongSuccess.php
+++ apps/frontend/modules/edits/templates/chosenSongSuccess.php
@@ -4,5 +4,20 @@ slot('h2', "<h2>Edits of $song</h2>"); ?>
 <p>All of the edits of the chosen song are listed below.
 Feel free to preview, download, play, and rate.</p>
 
-<?php $data = array('query' => $songs, 'what' => 'Song');
-//include_partial('edits/edit_table', $data) ?>
+<table id="edits">
+<caption>Edits of <?php echo $song ?></caption>
+<thead><tr>
+<th>User</th>
+<th>Stats</th>
+<th>Actions</th>
+</tr></thead>
+<tbody>
+<?php foreach ($songs as $z): ?>
+<tr>
+<td><?php echo $z->user_id ?></td>
+<td>Steps: <?php echo $z->steps ?></td>
+<td>Download</td>
+</tr>
+<?php endforeach; ?>
+</tbody>
+</table>
