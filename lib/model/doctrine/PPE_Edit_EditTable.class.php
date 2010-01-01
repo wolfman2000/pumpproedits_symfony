322bcb6818f016427da70cb349817df641fd23cb
Jason Felds <jafelds@gmail.com>
2010-01-01 12:10:09 -0500

    Add more columns.

--- lib/model/doctrine/PPE_Edit_EditTable.class.php
+++ lib/model/doctrine/PPE_Edit_EditTable.class.php
@@ -5,12 +5,12 @@ class PPE_Edit_EditTable extends Doctrine_Table
   public function getEditsBySong($songid)
   {
     $cols = 'diff, steps, jumps, holds, mines, trips, rolls, fakes, lifts';
-    $cols .= ', user_id, b.name uname, title';
+    $cols .= ', user_id, b.name uname, title, is_single';
     return $this->createQuery('a')
       ->select($cols)
       ->innerJoin('a.PPE_User_User b')
       ->where('song_id = ?', $songid)
-      ->orderBy('b.lc_name')
+      ->orderBy('b.lc_name, a.title')
       ->execute();
       
   }
