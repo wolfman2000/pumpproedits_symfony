ca69b4fce396027751f150772d14a53019e4ce77
Jason Felds <jafelds@gmail.com>
2010-01-01 14:00:14 -0500

    Use partials. Note: unsure why git thinks I'm renaming files here, but... *shrugs*

---
+++ apps/frontend/modules/edits/templates/_edit_table.php
@@ -0,0 +1,31 @@
+<table id="edits" summary="<?php echo $summary ?>">
+<caption><?php echo $caption ?></caption>
+<thead><tr>
+<?php if (isset($showuser)): ?><th>User</th><?php endif; ?>
+<th>Title</th>
+<th>Stats</th>
+<th>Actions</th>
+</tr></thead>
+<tbody>
+<?php foreach ($query as $z): ?>
+<tr>
+<?php if (isset($showuser)): ?>
+<td><?php echo link_to($z->uname, "@edit_cuser?id=$z->user_id") ?></td>
+<?php endif; ?>
+<td><?php echo $z->title ?></td>
+<td>
+<dl>
+<dt>Style</dt><dd><?php echo $z->is_single ? "S" : "D" . $z->diff ?></dd>
+<dt>Steps</dt><dd><?php echo $z->steps ?></dd>
+<dt>Jumps</dt><dd><?php echo $z->jumps ?></dd>
+<dt>Holds</dt><dd><?php echo $z->holds ?></dd>
+<dt>Mines</dt><dd><?php echo $z->mines ?></dd>
+<dt>Trips</dt><dd><?php echo $z->trips ?></dd>
+<dt>Rolls</dt><dd><?php echo $z->rolls ?></dd>
+</dl>
+</td>
+<td>Download</td>
+</tr>
+<?php endforeach; ?>
+</tbody>
+</table>
--- apps/frontend/modules/edits/templates/chosenSongSuccess.php
+++ apps/frontend/modules/edits/templates/chosenSongSuccess.php
@@ -4,32 +4,7 @@ slot('h2', "<h2>Edits of $song</h2>"); ?>
 <p>All of the edits of the chosen song are listed below.
 Feel free to preview, download, play, and rate.</p>
 
-<table id="edits">
-<caption>Edits of <?php echo $song ?></caption>
-<thead><tr>
-<th>User</th>
-<th>Title</th>
-<th>Stats</th>
-<th>Actions</th>
-</tr></thead>
-<tbody>
-<?php foreach ($songs as $z): ?>
-<tr>
-<td><?php echo link_to($z->uname, "@edit_cuser?id=$z->user_id") ?></td>
-<td><?php echo $z->title ?></td>
-<td>
-<dl>
-<dt>Style</dt><dd><?php echo $z->is_single ? "S" : "D" . $z->diff ?></dd>
-<dt>Steps</dt><dd><?php echo $z->steps ?></dd>
-<dt>Jumps</dt><dd><?php echo $z->jumps ?></dd>
-<dt>Holds</dt><dd><?php echo $z->holds ?></dd>
-<dt>Mines</dt><dd><?php echo $z->mines ?></dd>
-<dt>Trips</dt><dd><?php echo $z->trips ?></dd>
-<dt>Rolls</dt><dd><?php echo $z->rolls ?></dd>
-</dl>
-</td>
-<td>Download</td>
-</tr>
-<?php endforeach; ?>
-</tbody>
-</table>
+<?php $data = array('showuser' => 1, 'query' => $songs,
+  'caption' => "Edits of $song",
+  'summary' => "All of the edits for the song $song");
+include_partial('edits/edit_table', $data);
