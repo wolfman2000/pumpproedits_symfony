2802dc72ba2b67d1a5a1a547ed8794c61624db7a
Jason Felds <jafelds@gmail.com>
2010-01-01 12:10:21 -0500

    Add stat definition list.

--- apps/frontend/modules/edits/templates/chosenSongSuccess.php
+++ apps/frontend/modules/edits/templates/chosenSongSuccess.php
@@ -17,7 +17,17 @@ Feel free to preview, download, play, and rate.</p>
 <tr>
 <td><?php echo link_to($z->uname, "@edit_cuser?id=$z->user_id") ?></td>
 <td><?php echo $z->title ?></td>
-<td>Steps: <?php echo $z->steps ?></td>
+<td>
+<dl>
+<dt>Style</dt><dd><?php echo $z->is_single ? "S" : "D" . $z->diff ?></dd>
+<dt>Steps</dt><dd><?php echo $z->steps ?></dd>
+<dt>Jumps</dt><dd><?php echo $z->jumps ?></dd>
+<dt>Holds</dt><dd><?php echo $z->holds ?></dd>
+<dt>Mines</dt><dd><?php echo $z->mines ?></dd>
+<dt>Trips</dt><dd><?php echo $z->trips ?></dd>
+<dt>Rolls</dt><dd><?php echo $z->rolls ?></dd>
+</dl>
+</td>
 <td>Download</td>
 </tr>
 <?php endforeach; ?>
