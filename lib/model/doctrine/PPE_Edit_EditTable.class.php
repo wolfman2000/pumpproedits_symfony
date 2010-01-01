3c9e92cc6ed2f93e5b2ea77344c6c834809023be
Jason Felds <jafelds@gmail.com>
2010-01-01 11:31:39 -0500

    Put selected columns in variable.

--- lib/model/doctrine/PPE_Edit_EditTable.class.php
+++ lib/model/doctrine/PPE_Edit_EditTable.class.php
@@ -4,8 +4,10 @@ class PPE_Edit_EditTable extends Doctrine_Table
 {
   public function getEditsBySong($songid)
   {
+    $cols = 'diff, steps, jumps, holds, mines, trips, rolls, fakes, lifts';
+    $cols .= ', user_id';
     return $this->createQuery('a')
-      ->select('diff, steps, jumps, holds, mines, trips, rolls, fakes, lifts')
+      ->select($cols)
       ->where('song_id = ?', $songid)
       ->execute();
       
