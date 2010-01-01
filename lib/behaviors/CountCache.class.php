f27d78a18e590c16456305a2c2d44c9b84fa18b3
Jason Felds <jafelds@gmail.com>
2009-12-31 23:15:35 -0500

    Attempt CountCache behavior.

---
+++ lib/behaviors/CountCache.class.php
@@ -0,0 +1,29 @@
+class CountCache extends Doctrine_Template
+{
+protected $_options = array(
+    'relations' => array()
+  );
+ 
+  public function setTableDefinition()
+  {
+    foreach ($this->_options['relations'] as $relation => $options)
+    {
+      // Build column name if one is not given
+      if (!isset($options['columnName']))
+      {
+        $this->_options['relations'][$relation]['columnName'] = 'num_'.Doctrine_Inflector::tableize($relation);
+      }
+ 
+      // Add the column to the related model
+      $columnName = $this->_options['relations'][$relation]['columnName'];
+      $relatedTable = $this->_table->getRelation($relation)->getTable();
+      $this->_options['relations'][$relation]['className'] = $relatedTable->getOption('name');
+      $relatedTable->setColumn($columnName, 'integer', null, array('default' => 0));
+
+      #this->addListener(new CountCacheListener($this->_options));
+    }
+  }
+  public function setUp()
+  {
+  }
+}
---
+++ lib/behaviors/CountCacheListener.class.php
@@ -0,0 +1,66 @@
+class CountCacheListener extends Doctrine_Record_Listener
+{
+  protected $_options;
+ 
+  public function __construct(array $options)
+  {
+    $this->_options = $options;
+  }
+
+  public function postInsert(Doctrine_Event $event)
+  {
+    $invoker = $event->getInvoker();
+    foreach ($this->_options['relations'] as $relation => $options)
+    {
+      $table = Doctrine::getTable($options['className']);
+      $relation = $table->getRelation($options['foreignAlias']);
+ 
+      $table
+        ->createQuery()
+        ->update()
+        ->set($options['columnName'], $options['columnName'].' + 1')
+        ->where($relation['local'].' = ?', $invoker->$relation['foreign'])
+        ->execute();
+    }
+  }
+  public function postDelete(Doctrine_Event $event)
+  {
+    $invoker = $event->getInvoker();
+    foreach ($this->_options['relations'] as $relation => $options)
+    {
+      $table = Doctrine::getTable($options['className']);
+      $relation = $table->getRelation($options['foreignAlias']);
+ 
+      $table
+        ->createQuery()
+        ->update()
+        ->set($options['columnName'], $options['columnName'].' - 1')
+        ->where($relation['local'].' = ?', $invoker->$relation['foreign'])
+        ->execute();
+    }
+  }
+  public function preDqlDelete(Doctrine_Event $event)
+  {
+    foreach ($this->_options['relations'] as $relation => $options)
+    {
+      $table = Doctrine::getTable($options['className']);
+      $relation = $table->getRelation($options['foreignAlias']);
+ 
+      $q = clone $event->getQuery();
+      $q->select($relation['foreign']);
+      $ids = $q->execute(array(), Doctrine::HYDRATE_NONE);
+ 
+      foreach ($ids as $id)
+      {
+        $id = $id[0];
+ 
+        $table
+          ->createQuery()
+          ->update()
+          ->set($options['columnName'], $options['columnName'].' - 1')
+          ->where($relation['local'].' = ?', $id)
+          ->execute();
+      }
+    }
+
+}
