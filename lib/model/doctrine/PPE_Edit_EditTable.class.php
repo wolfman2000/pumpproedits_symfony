c2dd8813e1adaf5e0e9208757ca90de1a292de8b
Jason Felds <jafelds@gmail.com>
2010-01-01 11:26:00 -0500

    Wrong column.

--- lib/model/doctrine/PPE_Edit_EditTable.class.php
+++ lib/model/doctrine/PPE_Edit_EditTable.class.php
@@ -5,7 +5,7 @@ class PPE_Edit_EditTable extends Doctrine_Table
   public function getEditsBySong($songid)
   {
     return $this->createQuery('a')
-      ->select('diff, steps, jumps, holds, mines, trips, hands, fakes, lifts')
+      ->select('diff, steps, jumps, holds, mines, trips, rolls, fakes, lifts')
       ->where('song_id = ?', $songid)
       ->execute();
       
