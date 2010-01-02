48ca448fd5ac5bce5bee38398715a9cd84e622ce
Jason Felds <jafelds@gmail.com>
2010-01-02 01:35:16 -0500

    Not going to delete records. No point in keeping the deleting functions.

--- lib/behaviors/CountCacheListener.class.php
+++ lib/behaviors/CountCacheListener.class.php
@@ -55,45 +55,4 @@ class CountCacheListener extends Doctrine_Record_Listener
       }
     }
   }
-
-  public function postDelete(Doctrine_Event $event)
-  {
-    $invoker = $event->getInvoker();
-    foreach ($this->_options['relations'] as $relation => $options)
-    {
-      $table = Doctrine::getTable($options['className']);
-      $relation = $table->getRelation($options['foreignAlias']);
- 
-      $table
-        ->createQuery()
-        ->update()
-        ->set($options['columnName'], $options['columnName'].' - 1')
-        ->where($relation['local'].' = ?', $invoker->$relation['foreign'])
-        ->execute();
-    }
-  }
-  public function preDqlDelete(Doctrine_Event $event)
-  {
-    foreach ($this->_options['relations'] as $relation => $options)
-    {
-      $table = Doctrine::getTable($options['className']);
-      $relation = $table->getRelation($options['foreignAlias']);
- 
-      $q = clone $event->getQuery();
-      $q->select($relation['foreign']);
-      $ids = $q->execute(array(), Doctrine::HYDRATE_NONE);
- 
-      foreach ($ids as $id)
-      {
-        $id = $id[0];
- 
-        $table
-          ->createQuery()
-          ->update()
-          ->set($options['columnName'], $options['columnName'].' - 1')
-          ->where($relation['local'].' = ?', $id)
-          ->execute();
-      }
-    }
-  }
 }
