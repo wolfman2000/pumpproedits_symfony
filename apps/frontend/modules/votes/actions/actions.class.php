df42506ac37292d063569ffeb038deb83086962d
Jason Felds <jafelds@gmail.com>
2010-01-03 11:04:51 -0500

    The new voting modules.

---
+++ apps/frontend/modules/votes/actions/actions.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * votes actions.
+ *
+ * @package    pumpproedits
+ * @subpackage votes
+ * @author     JasonWolfman2000Felds
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class votesActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->forward('default', 'module');
+  }
+}
