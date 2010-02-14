dc191f3dfcd83648f94518f1e1fbe7d9b8714c46
Jason Felds <jafelds@gmail.com>
2010-02-14 16:11:40 -0500

    Add most of the form processing.

--- apps/frontend/modules/help/actions/actions.class.php
+++ apps/frontend/modules/help/actions/actions.class.php
@@ -31,7 +31,45 @@ class helpActions extends sfActions
     $this->form->bind($request->getParameter('validate'));
     if ($this->form->isValid())
     {
-    
+      // Check the things the form can't do through the database.
+      $table = Doctrine::getTable('PPE_User_User');
+      $data = array();
+
+      /* Check if the email is taken. */
+      $email = $this->form->getValue('email');
+      $id = $table->getIDByEmail($email);
+      if (!$id)
+      {
+        array_push($data, "There is no one with this email account.");
+      }
+      // Make sure the user isn't banned.
+      elseif (Doctrine::getTable('PPE_User_Role')->getIsUserBanned($id))
+      {
+        $data = array("You are prohibited from joining again.");
+        $this->noshow = 1;
+      }
+      
+      if (count($data))
+      {
+        $this->getResponse()->setStatusCode(409);
+        $this->data = $data;
+        return sfView::ERROR;
+      }
+      $username = $table->getNameByID($id);
+      
+      if ($this->form->getValue('choice') === "resend")
+      {
+        $table->confirmUser($id, 0);
+        $table = Doctrine::getTable('PPE_User_Condiment');
+        $table->updateOregano($id);
+        $mailer = new ResendConfirmationMessage($email, $username, $table->getOregano($id));
+      }
+      else
+      {
+        $mailer = null;
+      }
+      
+      $this->getMailer()->send($mailer);
     }
     else
     {
---
+++ lib/email/ResendConfirmationMessage.class.php
@@ -0,0 +1,26 @@
+<?php
+
+class RegisterConfirmationMessage extends BaseMessage
+{
+  public function __construct($to, $name, $confirm)
+  {
+    $subject = 'Pump Pro Edits - Reconfirming Account';
+    $body = <<<EOL
+According to our records, you have requested a new confirmation
+email message from Pump Pro Edits at www.pumpproedits.com recently.
+
+If you are that person, please go to the following URL:
+http://www.pumpproedits.com/confirm/%s
+
+Remember to also put your password in the form provided.
+
+If you did not request to reconfirm your account,
+you may delete this email and not worry about it.
+
+EOL;
+    parent::__construct($subject, sprintf($body, $confirm));
+    $this->setFrom(array('jafelds@gmail.com' => 'Jason "Wolfman2000" Felds'));
+    $this->setTo(array($to => $name));
+    $this->setBcc(array('jafelds@gmail.com' => 'Jason "Wolfman2000" Felds'));
+  }
+}
