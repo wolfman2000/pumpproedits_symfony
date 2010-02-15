32981caccf7257294fd0909cc91e76c8dc5782be
Jason Felds <jafelds@gmail.com>
2010-02-15 10:00:26 -0500

    Finally deal with edit uploading.

---
+++ apps/frontend/modules/upload/actions/actions.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * upload actions.
+ *
+ * @package    pumpproedits
+ * @subpackage upload
+ * @author     JasonWolfman2000Felds
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class uploadActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->forward('default', 'module');
+  }
+}
