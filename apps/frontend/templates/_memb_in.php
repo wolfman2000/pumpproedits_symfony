7c3e57f464ce602264add9acd1be28f41ec566cd
Jason Felds <jafelds@gmail.com>
2010-02-17 10:22:50 -0500

    Add public link.

--- apps/frontend/templates/_memb_in.php
+++ apps/frontend/templates/_memb_in.php
@@ -1,4 +1,5 @@
 <ul>
 <li><?php echo link_to('Upload Edit', '@upload_get') ?></li>
+<li><?php echo link_to('Edit Charter', '@chart_adv_get') ?></li>
 <li><?php echo link_to('Log Out', '@logout') ?></li>
 </ul>
