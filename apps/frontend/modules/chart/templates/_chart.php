fe226c3916b3776c9128d161758377f4c959e3ec
Jason Felds <jafelds@gmail.com>
2010-02-20 15:10:24 -0500

    Use paragraph tags in the sections this time.

--- apps/frontend/modules/chart/templates/_chart.php
+++ apps/frontend/modules/chart/templates/_chart.php
@@ -5,27 +5,23 @@ echo form_tag($route, "multipart=true"); ?>
   <fieldset>
     <legend>Select the edit to preview.</legend>
     <section>
-    <dl>
-      <dt><?php echo $form['edits']->renderLabel() ?></dt>
-      <dd><?php echo $form['edits'] ?></dd>
-      <dt><?php echo $form['file']->renderLabel() ?></dt>
-      <dd>
+      <p><?php echo $form['edits']->renderLabel() ?></p>
+      <p><?php echo $form['edits'] ?></p>
+      <p><?php echo $form['file']->renderLabel() ?></p>
+      <p>
         <?php echo $form['file'] ?>
         <button id="validate_rm_file" value="Nevermind" name="validate[rm_file]" type="button">Nevermind</button>
-        </dd>
-    </dl>
+      </p>
     </section>
     <section>
-    <dl>
-      <dt><?php echo $form['kind']->renderLabel() ?></dt>
-      <dd><?php echo $form['kind'] ?></dd>
-      <dt><?php echo $form['red4']->renderLabel() ?></dt>
-      <dd><?php echo $form['red4'] ?></dd>
-      <dt><?php echo $form['speed']->renderLabel() ?></dt>
-      <dd><?php echo $form['speed'] ?></dd>
-      <dt><?php echo $form['mpcol']->renderLabel() ?></dt>
-      <dd><?php echo $form['mpcol'] ?></dd>
-    </dl>
+    <p><?php echo $form['kind']->renderLabel() ?></p>
+    <p><?php echo $form['kind'] ?></p>
+    <p><?php echo $form['red4']->renderLabel() ?></p>
+    <p><?php echo $form['red4'] ?></p>
+    <p><?php echo $form['speed']->renderLabel() ?></p>
+    <p><?php echo $form['speed'] ?></p>
+    <p><?php echo $form['mpcol']->renderLabel() ?></p>
+    <p><?php echo $form['mpcol'] ?></p>
     </section>
   <p><button name="submit" id="submit" type="submit" value="submit">Submit!</button></p>
   </fieldset>
