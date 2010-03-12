cbf99a58257bf8d76f586a6e858af743452e76a7
Jason Felds <jafelds@gmail.com>
2010-03-11 22:02:16 -0500

    My hammy attempt at getting interest on entry.

--- apps/frontend/modules/ppehome/templates/indexSuccess.php
+++ apps/frontend/modules/ppehome/templates/indexSuccess.php
@@ -1,2 +1,15 @@
-<p>This is a temporary entry page until I can come up with
-something better. I may need some help in that regard.</p>
+<p>
+Welcome to the new home of the <abbr title="In The Groove">ITG</abbr>
+edit database. There is no question within the music game community
+that ITG is still a popular and important game within our history.
+This page serves to recognize its contributions to the edit data
+that many users created on the former ITGFreak.</p>
+<p>
+Note that unlike its <a href="http://www.pumpproedits.com">Pump Pro cousin</a>,
+there is no membership functionality at this time. This page is primarily
+to view the charts of both what the authors and users made. The
+base edits are provided in case you want to try your hand at it, but
+for now, individual edits will have to be <a href="mailto:jafelds@gmail.com">emailed
+in</a> to be included.
+</p>
+<p>With this in mind, have a look around and witness history!</p>
