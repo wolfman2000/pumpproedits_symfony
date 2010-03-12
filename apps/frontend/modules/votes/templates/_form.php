6d7bffbbcef9cbf5c4c8364e6c2f847320348709
Jason Felds <jafelds@gmail.com>
2010-03-11 21:56:22 -0500

    Not going to use this for now.

--- apps/frontend/modules/votes/templates/_form.php
+++
@@ -1,5 +0,0 @@
-<?php
-$params['form'] = $form;
-$params['route'] = '@rate_add_post';
-slot('legend', 'Only the rating is required.');
-include_partial("global/form_base", $params);
--- apps/frontend/modules/votes/templates/addSuccess.php
+++
@@ -1,6 +0,0 @@
-<?php slot('title', "Rate the edit — Pump Pro Edits");
-slot('h2', "<h2>Ratings on the edit</h2>"); ?>
-
-<p>Add or edit your rating for the edit below.</p>
-
-<?php include_partial('votes/form', array('form' => $form)) ?>
