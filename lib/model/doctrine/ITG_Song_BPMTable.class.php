5bdd260aeedf25239b6ee2079fca5613e6f9be20
Jason Felds <jafelds@gmail.com>
2010-03-11 21:27:32 -0500

    Add needed functions for edits.

--- lib/model/doctrine/ITG_Song_BPMTable.class.php
+++ lib/model/doctrine/ITG_Song_BPMTable.class.php
@@ -2,4 +2,8 @@
 
 class ITG_Song_BPMTable extends Doctrine_Table
 {
+  public function getBPMsBySongID($id)
+    {
+      return $this->findBySongId($id);
+    }
 }
--- lib/model/doctrine/ITG_Song_SongTable.class.php
+++ lib/model/doctrine/ITG_Song_SongTable.class.php
@@ -2,4 +2,14 @@
 
 class ITG_Song_SongTable extends Doctrine_Table
 {
+  public function getIDBySong($song)
+  {
+    return $this->createQuery('a')->select('id')->where('name = ?', $song)
+      ->fetchOne()->id;
+  }
+  public function getSongRow($songid)
+  {
+    return $this->createQuery('a')->find($songid)->execute();
+  }
+  
 }
