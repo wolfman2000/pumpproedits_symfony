047f654eb24958efb3332d20389522c50ebeff29
Jason Felds <jafelds@gmail.com>
2010-03-13 17:45:01 -0500

    Put this up just in case it's required.

---
+++ apps/frontend/modules/chart/templates/quickError.php
@@ -0,0 +1,10 @@
+<?php slot('title', 'Chart Gen Failure — ITG Edits');
+slot('h2', '<h2 class="error_list">Chart Gen Failure!</h2>');
+if (isset($data)): ?>
+<p>There was an error while parsing the edit file.</p>
+<p class="error_list"><?php echo $data ?></p>
+<p>Please fix the error and try again.</p>
+<?php else: ?>
+<p>Are you sure you selected a song? Double check and try again.</p>
+<?php endif;
+include_partial('chart/official', array('form' => $form, 'route' => '@chart_off_post', 'legend' => 'Select the song to preview.')) ?>
