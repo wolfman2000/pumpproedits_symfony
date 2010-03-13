9a1dba4ec8a6f3276eca356a675ed2e1ef089006
Jason Felds <jafelds@gmail.com>
2010-03-13 15:27:15 -0500

    Display the user edits...kind of. Have to fix the SQL integrity data.

--- lib/model/doctrine/ITG_User_UserTable.class.php
+++ lib/model/doctrine/ITG_User_UserTable.class.php
@@ -2,4 +2,10 @@
 
 class ITG_User_UserTable extends Doctrine_Table
 {
+  public function getUsersWithEdits()
+  {
+    return $this->createQuery('a')->select('name core, num_edits')
+      ->where('num_edits > ?', 0)
+      ->orderBy('lc_name')->execute();
+  }
 }
