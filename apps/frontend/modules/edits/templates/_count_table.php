1edd8c886b2ec19e67f305dd74f2a880d2e461fd
Jason Felds <jafelds@gmail.com>
2010-03-13 16:25:30 -0500

    Remove old markup.

--- apps/frontend/modules/edits/templates/_count_table.php
+++ apps/frontend/modules/edits/templates/_count_table.php
@@ -3,23 +3,4 @@
 <p><span><?php echo link_to($b->core, "@edit_c$what?id=$b->id") ?></span>
 <?php echo $b->num_edits ?></p>
 <?php endforeach; ?>
-
 </section>
-<!--
-<table id="count_table" summary="Number of edits by $what">
-  <caption>Edit count by <?php echo $what ?></caption>
-  <thead><tr>
-    <th><?php echo $what ?></th>
-    <th>Edit Count</th>
-  </tr></thead>
-  <?php $what = strtolower($what) ?>
-  <tbody>
-    <?php # foreach ($query as $b): ?>
-    <tr>
-      <td><?php # echo link_to($b->core, "@edit_c$what?id=$b->id") ?></td>
-      <td><?php # echo $b->num_edits ?></td>
-    </tr>
-    <?php # endforeach; ?>
-  </tbody>
-</table>
--->
