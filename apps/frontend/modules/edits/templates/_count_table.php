984a768320f6662af19cb8a25060716da38bac59
Jason Felds <jafelds@gmail.com>
2010-03-13 16:19:13 -0500

    Take advantage of CSS3.

--- apps/frontend/modules/edits/templates/_count_table.php
+++ apps/frontend/modules/edits/templates/_count_table.php
@@ -1,3 +1,11 @@
+<section id="multiCol">
+<?php $what = strtolower($what); foreach ($query as $b): ?>
+<p><span><?php echo link_to($b->core, "@edit_c$what?id=$b->id") ?></span>
+<?php echo $b->num_edits ?></p>
+<?php endforeach; ?>
+
+</section>
+<!--
 <table id="count_table" summary="Number of edits by $what">
   <caption>Edit count by <?php echo $what ?></caption>
   <thead><tr>
@@ -6,11 +14,12 @@
   </tr></thead>
   <?php $what = strtolower($what) ?>
   <tbody>
-    <?php foreach ($query as $b): ?>
+    <?php # foreach ($query as $b): ?>
     <tr>
-      <td><?php echo link_to($b->core, "@edit_c$what?id=$b->id") ?></td>
-      <td><?php echo $b->num_edits ?></td>
+      <td><?php # echo link_to($b->core, "@edit_c$what?id=$b->id") ?></td>
+      <td><?php # echo $b->num_edits ?></td>
     </tr>
-    <?php endforeach; ?>
+    <?php # endforeach; ?>
   </tbody>
 </table>
+-->
