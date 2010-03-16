861ab0ffb035d294a37ec6789dbc6d8d0fa5ba9b
Jason Felds <jafelds@gmail.com>
2010-03-16 16:27:04 -0400

    Finally offer content on the entry page.

--- apps/frontend/modules/ppehome/templates/indexSuccess.php
+++ apps/frontend/modules/ppehome/templates/indexSuccess.php
@@ -1,2 +1,11 @@
-<p>This is a temporary entry page until I can come up with
-something better. I may need some help in that regard.</p>
+<p>Welcome to the Pump Pro Edit database. Inside here, you will find
+many edits that dance players such as yourself have created, along
+with official charts that Pump creator Andamiro made themselves.</p>
+
+<p>Want to make an edit yourself? Then 
+<a href="java/PPEdits.zip">get the unofficial editor</a>,
+extract all of the files, and run the JAR file inside. You will
+be able to make an edit for any song using the drop down menus
+and the mouse.</p>
+
+<p>With that in mind, I hope you have fun here.</p>
