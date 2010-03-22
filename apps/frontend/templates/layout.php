34fa4d339ee46015c349e7e83395c7fee7f36539
Jason Felds <jafelds@gmail.com>
2010-03-22 07:38:44 -0400

    Put in "best used by Firefox" phrase. It's seriously getting to the point where keeping WebKit's hack is not worth it. I won't remove it right away, however.

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -66,6 +66,7 @@ echo $sf_content ?>
 </li>
 </ul>
 </nav>
-<footer>This website is © <a href="mailto:jafelds@gmail.com">Jason “Wolfman2000” Felds</a></footer>
+<footer>This website is ©2009-2010 <a href="mailto:jafelds@gmail.com">Jason “Wolfman2000” Felds</a>.<br />
+This website works best in <a href="http://www.firefox.com">Firefox</a>.</footer>
 </body>
 </html>
