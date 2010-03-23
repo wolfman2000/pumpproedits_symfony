e898d3a3239705d47eb626adcfde2564ede16695
Jason Felds <jafelds@gmail.com>
2010-03-22 21:49:51 -0400

    Get the song list.

--- apps/frontend/modules/create/actions/actions.class.php
+++ apps/frontend/modules/create/actions/actions.class.php
@@ -22,6 +22,7 @@ class createActions extends sfActions
       $this->getResponse()->setStatusCode(415);
       return sfView::ERROR;
     }
+    $this->songs = Doctrine::getTable('PPE_Song_Song')->getSongs();
     $this->getResponse()->setHttpHeader('Content-Type', 'application/xhtml+xml');
   }
 }
