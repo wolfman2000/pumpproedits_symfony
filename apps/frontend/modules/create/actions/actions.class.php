9336dad3f481bb8797e0525de7792f28283cc744
Jason Felds <jafelds@gmail.com>
2010-03-23 13:04:07 -0400

    Return the rest of the data.

--- apps/frontend/modules/create/actions/actions.class.php
+++ apps/frontend/modules/create/actions/actions.class.php
@@ -41,6 +41,22 @@ class createActions extends sfActions
     
     $ret['name'] = $sRow->name;
     $ret['measures'] = $sRow->measures;
+    
+    $bpms = Doctrine::getTable('PPE_Song_BPM')->getBPMsBySongID($id);
+    $bArr = array();
+    foreach ($bpms as $b)
+    {
+      $bArr[] = array('beat' => $b->beat, 'bpm' => $b->bpm);
+    }
+    $ret['bpms'] = $bArr;
+    
+    $stps = Doctrine::getTable('PPE_Song_Stop')->getStopsBySongID($id);
+    $sArr = array();
+    foreach ($stps as $s)
+    {
+      $sArr[] = array('beat' => $s->beat, 'break' => $s->break . "B");
+    }
+    $ret['stps'] = $sArr;
 
     return $this->renderText(json_encode($ret));
   }
