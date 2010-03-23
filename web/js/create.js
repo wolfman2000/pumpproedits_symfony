6c60b82eb813741fc8d3cb9c0f07b1b8c044b9ee
Jason Felds <jafelds@gmail.com>
2010-03-23 11:44:28 -0400

    More preparation for AJAX.

--- web/js/create.js
+++ web/js/create.js
@@ -1,14 +1,29 @@
 var isDirty; // has the work changed? Should a prompt for saving take place?
 var measures; // What does the internal note structure look like?
 var columns; // How many columns are we working with?
+var songID;
 var songs;
 var gNote;
+var songData;
 
+function editMode()
+{
+  songID = 7;
+  $.ajax({
+    url: "/create/song/" + songID, cache: true,
+    contentType: "application/xml",
+    dataType: "xml", success: function(data)
+    {
+      songData = data;
+    },
+  });
+}
 
 function init()
 {
   $("nav *.edit").hide();
   $("nav *.choose").show();
+  $("#stylelist").attr("disabled", "disabled");
   $("article > svg").attr("width", "224px");
   $("article > svg").attr("height", "448px");
   
