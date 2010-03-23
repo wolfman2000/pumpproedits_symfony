59f240fdc88d427777c2a99c9d7b95f4742a90f5
Jason Felds <jafelds@gmail.com>
2010-03-23 15:58:22 -0400

    Hardcode measure generation. Just about everything else will have to be hard coded too. :(

--- web/js/create.js
+++ web/js/create.js
@@ -13,6 +13,77 @@ const SVG_BG = "white"; // background of the SVG element and other key things.
 
 const MEASURE_HEIGHT = ARR_HEIGHT * SCALE * BEATS_PER_MEASURE; // the height of our measure.
 
+function genMeasure(x, y)
+{
+  var s = document.createElementNS(SVG_NS, "svg");
+  s.setAttribute("x", x);
+  s.setAttribute("y", y);
+  
+  var r1 = document.createElementNS(SVG_NS, "rect");
+  r1.setAttribute("x", 0);
+  r1.setAttribute("y", 0);
+  r1.setAttribute("height", ARR_HEIGHT * SCALE);
+  r1.setAttribute("width", columns * ARR_HEIGHT * SCALE);
+  r1.setAttribute("fill", SVG_BG);
+  s.appendChild(r1);
+  
+  var r2 = document.createElementNS(SVG_NS, "rect");
+  r2.setAttribute("x", 0);
+  r2.setAttribute("y", ARR_HEIGHT * SCALE);
+  r2.setAttribute("height", ARR_HEIGHT * SCALE);
+  r2.setAttribute("width", columns * ARR_HEIGHT * SCALE);
+  r2.setAttribute("fill", "#dcdcdc");
+  s.appendChild(r2);
+  
+  var r3 = document.createElementNS(SVG_NS, "rect");
+  r3.setAttribute("x", 0);
+  r3.setAttribute("y", ARR_HEIGHT * SCALE * 2);
+  r3.setAttribute("height", ARR_HEIGHT * SCALE);
+  r3.setAttribute("width", columns * ARR_HEIGHT * SCALE);
+  r3.setAttribute("fill", SVG_BG);
+  s.appendChild(r3);
+  
+  var r4 = document.createElementNS(SVG_NS, "rect");
+  r4.setAttribute("x", 0);
+  r4.setAttribute("y", ARR_HEIGHT * SCALE * 3);
+  r4.setAttribute("height", ARR_HEIGHT * SCALE);
+  r4.setAttribute("width", columns * ARR_HEIGHT * SCALE);
+  r4.setAttribute("fill", "#bebebe");
+  s.appendChild(r4);
+  
+  var l1 = document.createElementNS(SVG_NS, "line");
+  l1.setAttribute("x1", 0);
+  l1.setAttribute("y1", 0.1);
+  l1.setAttribute("x2", columns * ARR_HEIGHT * SCALE);
+  l1.setAttribute("y2", 0.1);
+  l1.setAttribute("fill", "black");
+  l1.setAttribute("stroke", "black");
+  l1.setAttribute("stroke-width", 0.1);
+  s.appendChild(l1);
+  
+  var l2 = document.createElementNS(SVG_NS, "line");
+  l2.setAttribute("x1", 0.05);
+  l2.setAttribute("y1", 0);
+  l2.setAttribute("x2", 0.05);
+  l2.setAttribute("y2", MEASURE_HEIGHT);
+  l2.setAttribute("fill", "black");
+  l2.setAttribute("stroke", "black");
+  l2.setAttribute("stroke-width", 0.1);
+  s.appendChild(l2);
+  
+  var l3 = document.createElementNS(SVG_NS, "line");
+  l3.setAttribute("x1", columns * ARR_HEIGHT * SCALE - 0.05);
+  l3.setAttribute("y1", 0);
+  l3.setAttribute("x2", columns * ARR_HEIGHT * SCALE - 0.05);
+  l3.setAttribute("y2", MEASURE_HEIGHT);
+  l3.setAttribute("fill", "black");
+  l3.setAttribute("stroke", "black");
+  l3.setAttribute("stroke-width", 0.1);
+  s.appendChild(l3);
+  
+  return s;
+}
+
 function getCols()
 {
   switch ($("#stylelist").val())
@@ -29,23 +100,14 @@ function editMode()
   $.getJSON("/create/song/" + songID, function(data)
   {
     songData = data;
-    $("article > svg").attr("height", MEASURE_HEIGHT * songData.measures + MEASURE_HEIGHT / 2);
+    $("article > svg").attr("height", MEASURE_HEIGHT * songData.measures + MEASURE_HEIGHT);
     columns = getCols();
     $("article > svg").attr("width", (columns + 2) * ARR_HEIGHT * SCALE);
     
     // append the measures.
     for (var i = 0; i < songData.measures; i++)
     {
-      var s = document.createElementNS(SVG_NS, 'svg');
-      s.setAttribute('x', ARR_HEIGHT * SCALE);
-      s.setAttribute('y', MEASURE_HEIGHT + MEASURE_HEIGHT * i);
-      
-      
-      var m = document.createElementNS(SVG_NS, 'use');
-      m.setAttribute('xlink:href', '/svg/arrowdef.svg#measure');
-      m.setAttribute('transform', 'scale(' + columns + ', 1)');
-      s.appendChild(m);
-      $("g#notes").append(s);
+      $("g#notes").append(genMeasure(ARR_HEIGHT * SCALE, ARR_HEIGHT * BEATS_PER_MEASURE + MEASURE_HEIGHT * i));
     }
     
   });
