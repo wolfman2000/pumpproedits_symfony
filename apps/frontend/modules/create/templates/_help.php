f2fb05e7a3bc361e25a572aeef12c6620bdb8a92
Jason Felds <jafelds@gmail.com>
2010-04-01 17:44:41 -0400

    Implement keyboard shortcuts section.

--- apps/frontend/modules/create/templates/_help.php
+++ apps/frontend/modules/create/templates/_help.php
@@ -3,7 +3,7 @@
 <head>
 <title>Editor Help</title>
 </head>
-<?php $back = "<h3><a href=\"#top\">Back to Top</a></h3>"; ?>
+<?php $back = "<h3><a href=\"#top\">Back to Top</a></h3>\r\n</section>"; ?>
 <body>
 <h1>Edit Creator Help</h1>
 <ul id="top">
@@ -35,6 +35,42 @@ once an edit is validated and there are no errors.</dd>
 Submitting is only possible once an edit is validated and there are no errors.</dd>
 </dl>
 <?php echo $back; ?>
-</section>
+<section id="song">
+<h2>Song Mode</h2>
+<?php echo $back; ?>
+<section id="edit">
+<h2>Edit Mode</h2>
+<?php echo $back; ?>
+<section id="keyb">
+<h2>Keyboard Shortcuts</h2>
+<p>This online editor works best when using both the keyboard and mouse.
+The list of the keyboard controls is below. You can assume
+that letter casing doesn't matter unless otherwise specified.</p>
+<dl>
+<?php $sync = "Change the sync to use %s notes.";
+$type = "Change the arrow type to %s notes.";
+$keys = array('1' => sprintf($sync, '4th'),
+'2' => sprintf($sync, '8th'),
+'3' => sprintf($sync, '12th'),
+'4' => sprintf($sync, '16th'),
+'5' => sprintf($sync, '24th'),
+'6' => sprintf($sync, '32nd'),
+'7' => sprintf($sync, '48th'),
+'8' => sprintf($sync, '64th'),
+'9' => sprintf($sync, '192nd'),
+'T' => sprintf($type, "Tap"),
+'H' => sprintf($type, 'Hold'),
+'E' => sprintf($type, 'Hold/Roll End'),
+'R' => sprintf($type, 'Roll'),
+'M' => sprintf($type, 'Mine'),
+'L' => sprintf($type, 'Lift'),
+'F' => sprintf($type, 'Fake'),
+);
+foreach ($keys as $k => $v): ?>
+<dt><?php echo $k; ?></dt>
+<dd><?php echo $v; ?></dd>
+<?php endforeach; ?>
+</dl>
+<?php echo $back; ?>
 </body>
 </html>
