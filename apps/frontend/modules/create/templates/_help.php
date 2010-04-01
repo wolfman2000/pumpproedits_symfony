bc2eb7b86e20c02396cfd329e86a6a499e8d623b
Jason Felds <jafelds@gmail.com>
2010-04-01 19:04:29 -0400

    More help implementation.

--- apps/frontend/modules/create/templates/_help.php
+++ apps/frontend/modules/create/templates/_help.php
@@ -3,13 +3,17 @@
 <head>
 <title>Editor Help</title>
 </head>
-<?php $back = "<h3><a href=\"#top\">Back to Top</a></h3>\r\n</section>"; ?>
+<?php $back = "<h4><a href=\"#top\">Back to Top</a></h4>\r\n</section>"; ?>
 <body>
 <h1>Edit Creator Help</h1>
 <ul id="top">
 <li><a href="#core">Core Buttons</a></li>
 <li><a href="#song">Song Mode</a></li>
-<li><a href="#edit">Edit Mode</a></li>
+<li><a href="#edit">Edit Mode</a>
+<ul>
+<li><a href="#menu">Menu Options</a></li>
+<li><a href="#chrt">Chart Controls</a></li>
+</ul></li>
 <li><a href="#keyb">Keyboard Shortcuts</a></li>
 </ul>
 <section id="core">
@@ -32,14 +36,73 @@ Validation of the edit is required before submitting the edit.</dd>
 once an edit is validated and there are no errors.</dd>
 <dt>Submit</dt>
 <dd>Submit the current edit to your Pump Pro Edits account.
-Submitting is only possible once an edit is validated and there are no errors.</dd>
+Submitting is only possible once an edit is validated and there are no errors.
+You have to be logged in presently in order to submit an edit.</dd>
 </dl>
 <?php echo $back; ?>
 <section id="song">
 <h2>Song Mode</h2>
+<p>Song Mode is very simple. All that you have to do to start editing
+is to select both a song and a style using the drop down menus.
+You have to choose a song before you choose a style. Once you choose
+a style, it changes to Edit Mode. If you want to change the mode, you
+will have to either start a new edit or load an old edit.</p>
 <?php echo $back; ?>
 <section id="edit">
 <h2>Edit Mode</h2>
+<p>Edit Mode is where you actually create your own edit. Use
+the menus, keyboard, and mouse to select your options and
+place your arrows.</p>
+<?php echo $back; ?>
+<section id="menu">
+<h3>Menu Options</h3>
+<p>All of the options to control your edit are available here.</p>
+<?php $keys = array('Edit Name' => 'Enter the name of the edit. '
+  . 'This must be provided to allow validating/saving your edit.',
+'Diff. Rating' => 'Enter the numeric difficulty of the edit. '
+  . 'This must be provided to allow validating/saving your edit.',
+'Note Sync' => 'Decide what sync of notes you will be placing in your edit. '
+  . 'Most edits will need no more than 16th notes available at once.',
+'Note Type' => 'Decide what type of notes you will be placing in your edit. '
+  . 'More information about the types will be available later.',
+'Chart Zoom' => 'Decide how big or small the chart itself will look. '
+  . 'It is recommended to user higher zooms when working with more exact notes.',
+'Routine Player' => 'Decide which player\'s notes are in use. '
+  . 'This is only available when creating/editing Routine style edits.',
+); ?>
+<dl>
+<?php foreach ($keys as $k => $v): ?>
+<dt><?php echo $k; ?></dt>
+<dd><?php echo $v; ?></dd>
+<?php endforeach; ?>
+</dl>
+<p>In addition to the above, there are non interactive portions that
+may prove useful to you.</p>
+<?php $phr = "Shows the number of %s in the edit.";
+$keys = array('Measure' => "Indicates which measure your mouse is over. "
+  . "If your mouse is over two measures, the higher one takes precedence.",
+'Beat' => "Indicates which beat inside the measure your mouse is over. "
+  . "If the mouse was clicked at this position, an arrow would be placed at "
+  . "the corresponding measure and beat. Beats are 0-index based.",
+'Steps' => sprintf($phr, "steps"),
+'Jumps' => sprintf($phr, "jumps"),
+'Holds' => sprintf($phr, "holds"),
+'Mines' => sprintf($phr, "mines"),
+'Trips' => sprintf($phr, "hands/trips/three+ panel hits"),
+'Rolls' => sprintf($phr, "rolls"),
+'Lifts' => sprintf($phr, "lifts"),
+'Fakes' => sprintf($phr, "fakes"),
+);
+?>
+<dl>
+<?php foreach ($keys as $k => $v): ?>
+<dt><?php echo $k; ?></dt>
+<dd><?php echo $v; ?></dd>
+<?php endforeach; ?>
+</dl>
+<?php echo $back; ?>
+<section id="chrt">
+<h3>Chart Controls</h3>
 <?php echo $back; ?>
 <section id="keyb">
 <h2>Keyboard Shortcuts</h2>
