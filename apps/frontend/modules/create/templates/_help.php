360e71c88391152db4afe2439b466dc61acd3c05
Jason Felds <jafelds@gmail.com>
2010-04-01 17:18:32 -0400

    Add content.

--- apps/frontend/modules/create/templates/_help.php
+++ apps/frontend/modules/create/templates/_help.php
@@ -3,7 +3,38 @@
 <head>
 <title>Editor Help</title>
 </head>
+<?php $back = "<h3><a href=\"#top\">Back to Top</a></h3>"; ?>
 <body>
-<p>This is a temp page.</p>
+<h1>Edit Creator Help</h1>
+<ul id="top">
+<li><a href="#core">Core Buttons</a></li>
+<li><a href="#song">Song Mode</a></li>
+<li><a href="#edit">Edit Mode</a></li>
+<li><a href="#keyb">Keyboard Shortcuts</a></li>
+</ul>
+<section id="core">
+<h2>Core Buttons</h2>
+<dl>
+<dt>New</dt>
+<dd>Start a new edit. If you haven't saved/validated your edit recently,
+you will be asked if you want to do so first.</dd>
+<dt>Help</dt>
+<dd>Display this window.</dd>
+<dt>Load</dt>
+<dd>Load an edit. If you haven't saved/validated your edit recently,
+you will be asked if you want to do so first.</dd>
+<dt>Validate</dt>
+<dd>Validate the current edit. Validation is only possible if an edit
+rating and difficulty are both given, and the edit was recently changed.
+Validation of the edit is required before submitting the edit.</dd>
+<dt>Save</dt>
+<dd>Save the current edit to your hard drive. Saving is only possible
+once an edit is validated and there are no errors.</dd>
+<dt>Submit</dt>
+<dd>Submit the current edit to your Pump Pro Edits account.
+Submitting is only possible once an edit is validated and there are no errors.</dd>
+</dl>
+<?php echo $back; ?>
+</section>
 </body>
 </html>
