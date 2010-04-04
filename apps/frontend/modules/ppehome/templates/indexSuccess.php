bd518cea97c640f48072d07895d1acfbf8c5eb94
Jason Felds <jafelds@gmail.com>
2010-04-03 22:23:30 -0400

    Updated index page. Mention Pro 2.

--- apps/frontend/modules/ppehome/templates/indexSuccess.php
+++ apps/frontend/modules/ppehome/templates/indexSuccess.php
@@ -2,10 +2,8 @@
 many edits that dance players such as yourself have created, along
 with official charts that Pump creator Andamiro made themselves.</p>
 
-<p>Want to make an edit yourself? Then 
-<a href="java/PPEdits.zip">get the unofficial editor</a>,
-extract all of the files, and run the JAR file inside. You will
-be able to make an edit for any song using the drop down menus
-and the mouse.</p>
+<p>Want to make an edit yourself? Use the
+<?php echo link_to("Edit Creator", "@edit_creator"); ?> tool.</p>
 
-<p>With that in mind, I hope you have fun here.</p>
+<p>Please pardon any mess inside: Pump it up Pro 2 is coming, and
+I'm getting everything ready as soon as possible.</p>
