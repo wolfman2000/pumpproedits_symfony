b6d1c38c0d8c83bf7fe61af31169dbcccd85ddd0
Jason Felds <jafelds@gmail.com>
2010-04-12 12:37:19 -0400

    Disable old edit program for IE. Unsure whether or not to still allow base edits.

--- apps/frontend/templates/_nav_norm.php
+++ apps/frontend/templates/_nav_norm.php
@@ -8,6 +8,11 @@
 <li>
     <h4>Edits</h4>
     <ul>
+    <?php if (strpos($_SERVER['HTTP_USER_AGENT'], "MSIE") !== false): ?>
+    <li><?php echo link_to("Base Edit Files", '@base_edit') ?></li>
+    <?php else: ?>
+    <li><?php echo link_to("Edit Creator", '@edit_creator') ?></li>
+    <?php endif; ?>
 <?php /*
     <li><?php echo link_to("Base Edit Files", '@base_edit') ?></li>
     <li><?php echo link_to("Edit Stat Getter", '@edit_stat_get') ?></li>
@@ -21,11 +26,6 @@
 <li>
     <h4>Everyone</h4>
     <ul>
-    <?php if (strpos($_SERVER['HTTP_USER_AGENT'], "MSIE") !== false): ?>
-    <li><a href="/java/PPEdits.zip">Edit Maker (Java)</a></li>
-    <?php else: ?>
-    <li><?php echo link_to("Edit Creator", '@edit_creator') ?></li>
-    <?php endif; ?>
     <li><?php echo link_to("Contact", '@contact_get') ?></li>
     <li><?php echo link_to("Credits/Thanks", '@thanks') ?></li>
     </ul>
